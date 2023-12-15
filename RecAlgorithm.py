29bc25dae7da723348e5236e5f0037a90c0e2862
Adithya Rao <57645830+adithyarrao909@users.noreply.github.com>
2023-12-15 13:59:30 -0500

    +features
    Added popularity toggle and more UI interaction

--- RecAlgorithm.py
+++ RecAlgorithm.py
@@ -2,11 +2,18 @@
 
 import spacy
 
-def find_most_similar_rated_books(user_input, books, top_n=10):
+def find_most_similar_rated_books(user_input, books, popular_rec_toggle=0, top_n=10):
     nlp = spacy.load("en_core_web_md")
     user_input_embedding = nlp(user_input).vector
 
-    # Filter books with ratings
+    if(popular_rec_toggle):
+        # Filter books with # of ratings (popularity)
+        sorted_books = [book for book in books if "readinglog_count" in book]
+
+        # Take the top N most-reviewed books
+        books = sorted(sorted_books, key=lambda x: x.get("readinglog_count", 0), reverse=True)
+
+    # Filter books with rating avg
     rated_books = [book for book in books if "ratings_average" in book]
     
     # Sort books by ratings in descending order
@@ -15,16 +22,16 @@ def find_most_similar_rated_books(user_input, books, top_n=10):
     # Take the top N rated books
     top_rated_books = sorted_books[:top_n]
 
-
     most_similar_rated_books = []
 
     for book in top_rated_books:
-        if "title" in book and "author_name" in book and "subject_key" in book:
+        if "title" in book and "author_name" in book and "subject_key" in book and "subject_facet" in book:
             book_title = book["title"]
             book_embedding = nlp(book_title).vector
             similarity = (
                 nlp(" ".join(book["author_name"])).similarity(nlp(user_input)) +
-                nlp(" ".join(book["author_name"])).similarity(nlp(book_title)) +
+                nlp(" ".join(book["subject_key"])).similarity(nlp(user_input)) +
+                nlp(" ".join(book["subject_facet"])).similarity(nlp(user_input)) +
                 nlp(book_title).similarity(nlp(user_input))
             )
             most_similar_rated_books.append({"similarity": similarity, "book": book})
--- main.py
+++ main.py
@@ -8,20 +8,38 @@ import UserInterface
 from colorama import init, Fore, Style
 
 if __name__ == "__main__":
+    popular_rec_toggle = 0
     init(autoreset=True)  # Initialize colorama for colored output
-    
-    user_input = input("Enter a string: ")
-    books = API.get_open_library_books(user_input)
-    print(books)
+    while(1):
+        user_input = input("Would you like your books to be sorted by popularity?(y/n): ")
+        if (user_input in {"Y", "y"}):
+           popular_rec_toggle = 1
+           break
+        elif (user_input in {"N", "n"}):
+           popular_rec_toggle = 0
+           break
+        else:
+            print("Invalid input! Try again")
+    while(1):
+        pgcnt_user_input = input("How many pages? (0=0:250, 1=250:500, 2=500:10000): ")
+        if (pgcnt_user_input in {"0", "1", "2"}):
+            break
+        else:
+            print("Invalid input! Try again")
+
+    genre_user_input = input("What genre are you feeling? (try to be accurate with spelling): ")
+
+    user_input = input("Include any other keywords/descriptions of the movie you want separated by a space: ")
+
+    books = API.get_open_library_books(user_input + genre_user_input)
 
     if books:
         top_rated_books = sorted([book for book in books if "ratings_average" in book], key=lambda x: x.get("ratings_average", 0), reverse=True)[:100]
         top_rated_books = top_rated_books[:int(sysdefs.TOP_RATED)]
-        most_similar_rated_books = RecAlgorithm.find_most_similar_rated_books(user_input, books, top_n=int(sysdefs.TOP_RATED))
+        most_similar_rated_books = RecAlgorithm.find_most_similar_rated_books(user_input, books, popular_rec_toggle, top_n=int(sysdefs.TOP_RATED))
 
         if top_rated_books and most_similar_rated_books:
-            # UserInterface.ui(top_rated_books, most_similar_rated_books)
-            print("REMOVE THIS")
+            UserInterface.ui(top_rated_books, most_similar_rated_books)
         else:
             print(Fore.RED + "No similar and rated books found." + Style.RESET_ALL)
     else:
